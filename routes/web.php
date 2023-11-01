a1e8ed240631a591540c391926d6bcb2fe290a02
Aiman Ahmad <khairulaiman@Aimans-MacBook-Pro.local>
2023-11-01 22:21:59 +0800

    First commit

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\LecturerController;
+use App\Http\Controllers\LecturerController; 
+// Contoller
 
 /*
 |--------------------------------------------------------------------------
